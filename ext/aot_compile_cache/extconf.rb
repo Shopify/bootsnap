e84b0e0103a57a712c0a6883b26102dbda34cf06
Burke Libbey <burke@libbey.me>
2017-01-30 16:05:50 -0500

    use -std=c99

--- ext/aot_compile_cache/extconf.rb
+++ ext/aot_compile_cache/extconf.rb
@@ -1,5 +1,5 @@
 require "mkmf"
-$CFLAGS << ' -O3 -msse4.2'
+$CFLAGS << ' -O3 -msse4.2 -std=c99'
 have_header('x86intrin.h')
 $CFLAGS << ' -Wall -Wextra -Wpedantic -Werror'
 $CFLAGS << ' -Wno-unused-parameter' # VALUE self has to be there but we don't care what it is.
