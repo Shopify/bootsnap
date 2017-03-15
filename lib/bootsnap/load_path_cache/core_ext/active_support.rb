d2723452cdd34e7259164c7ec25f0782b7028a90
Burke Libbey <burke@libbey.me>
2017-03-15 15:37:27 -0400

    remove_constant

--- lib/bootsnap/load_path_cache/core_ext/active_support.rb
+++ lib/bootsnap/load_path_cache/core_ext/active_support.rb
@@ -1,5 +1,21 @@
 module ActiveSupport
   module Dependencies
+    class << self
+      def with_bootsnap_fallback(*errors)
+        yield
+      rescue *errors
+        bootsnap_slow_path { yield }
+      end
+
+      def bootsnap_slow_path
+        prev = Thread.current[:dependencies_try_harder] || false
+        Thread.current[:dependencies_try_harder] = true
+        yield
+      ensure
+        Thread.current[:dependencies_try_harder] = prev
+      end
+    end
+
     alias_method :search_for_file_without_cache, :search_for_file
     def search_for_file(path)
       if Thread.current[:dependencies_try_harder]
@@ -20,26 +36,24 @@ module ActiveSupport
     class << self
       alias_method :depend_on_without_cache, :depend_on
       def depend_on(file_name, message = "No such file to load -- %s.rb")
-        depend_on_without_cache(file_name, message)
-      rescue LoadError
-        begin
-          Thread.current[:dependencies_try_harder] = true
+        Dependencies.with_bootsnap_fallback(LoadError) do
           depend_on_without_cache(file_name, message)
-        ensure
-          Thread.current[:dependencies_try_harder] = false
         end
+      end
+    end
+
+    # We could do the fast lookup here too but it's not really a hot path.
+    alias_method :remove_constant_without_cache, :remove_constant
+    def remove_constant(const)
+      Dependencies.bootsnap_slow_path do
+        remove_constant_without_cache(const)
       end
     end
 
     alias_method :load_missing_constant_without_cache, :load_missing_constant
     def load_missing_constant(from_mod, const_name)
-      load_missing_constant_without_cache(from_mod, const_name)
-    rescue NameError
-      begin
-        Thread.current[:dependencies_try_harder] = true
+      Dependencies.with_bootsnap_fallback(NameError) do
         load_missing_constant_without_cache(from_mod, const_name)
-      ensure
-        Thread.current[:dependencies_try_harder] = false
       end
     end
   end
