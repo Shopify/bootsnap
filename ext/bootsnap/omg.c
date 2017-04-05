c95d2ba5a5cb0ca7c9b8132c77011e6d8f1af5bf
Burke Libbey <burke@libbey.me>
2017-04-05 15:56:35 -0400

    welp

--- ext/bootsnap/omg.c
+++ ext/bootsnap/omg.c
@@ -1,10 +1,13 @@
-#include "bootsnap.h"
+#include "ruby.h"
+
+#define GENSYM_CONCAT(name, salt) name ## salt
+#define GENSYM2(name, salt)       GENSYM_CONCAT(name, salt)
+#define GENSYM(name)              GENSYM2(name, __LINE__)
+#define SKIP_BYTES(n)             uint8_t GENSYM(_a)[(n)]
 
 typedef struct rb_iseq_location_struct {
   VALUE path;
-  VALUE absolute_path;
-  VALUE base_label;
-  VALUE label;
+  SKIP_BYTES(24);
   VALUE first_lineno;
 } rb_iseq_location_t;
 
@@ -14,77 +17,37 @@ struct iseq_line_info_entry {
 };
 
 struct rb_iseq_constant_body {
-  uint8_t type; /* discarded info */
-  unsigned int stack_max;
-  unsigned int local_size;
-  unsigned int iseq_size;
+  SKIP_BYTES(13);
   const VALUE *iseq_encoded;
-  uint8_t flags; /* discarded info */
-  unsigned int size;
-  int lead_num;
-  int opt_num;
-  int rest_start;
-  int post_start;
-  int post_num;
-  int block_start;
-  const VALUE *opt_table;
-  void *keyword;
+  SKIP_BYTES(46);
   rb_iseq_location_t location;
   const struct iseq_line_info_entry *line_info_table;
-  const ID *local_table;
-  const struct iseq_catch_table *catch_table;
-  const struct rb_iseq_struct *parent_iseq;
-  struct rb_iseq_struct *local_iseq;
-  union iseq_inline_storage_entry *is_entries;
-  struct rb_call_info *ci_entries;
-  struct rb_call_cache *cc_entries;
-  VALUE mark_ary;
-  unsigned int local_table_size;
-  unsigned int is_size;
-  unsigned int ci_size;
-  unsigned int ci_kw_size;
+  SKIP_BYTES(80);
   unsigned int line_info_size;
-}; /* MANY things inlined/elided in this definition */
+};
 
 typedef struct rb_iseq_struct {
-  VALUE flags;
-  VALUE reserved1;
+  SKIP_BYTES(16);
   struct rb_iseq_constant_body *body;
-
   /* ... (truncated) ... */
 } rb_iseq_t;
 
 typedef struct rb_control_frame_struct {
   const VALUE *pc;
-  VALUE *sp;
+  SKIP_BYTES(8);
   const void *iseq;
-  VALUE flag;
-  VALUE self;
-  VALUE *ep;
-  const void *block_iseq;
-  VALUE proc;
-} rb_control_frame_t; /* pretty much accurate definition */
+  SKIP_BYTES(40);
+} rb_control_frame_t; /* can't truncate because we need size */
 
 typedef struct rb_thread_struct {
-  void *list_node_next; /* (inlined) */
-  void *list_node_prev; /* (inlined) */
+  SKIP_BYTES(16);
   VALUE self;
-  void *vm; /* (voided type) */
-
+  SKIP_BYTES(8);
   VALUE *stack;
   size_t stack_size;
   rb_control_frame_t *cfp;
-
   /* ... (truncated) ... */
 } rb_thread_t;
-
-static rb_thread_t *
-get_thread()
-{
-  VALUE th_v;
-  th_v = rb_funcall(rb_cThread, rb_intern("current"), 0);
-  return RTYPEDDATA_DATA(th_v);
-}
 
 static unsigned int
 get_line_no(const rb_iseq_t *iseq, size_t pos)
@@ -108,17 +71,8 @@ get_line_no(const rb_iseq_t *iseq, size_t pos)
   return (&table[i-1])->line_no;
 }
 
-static VALUE
-handle_iseq(rb_control_frame_t *cfp)
-{
-  const rb_iseq_t *iseq = cfp->iseq;
-  VALUE path = iseq->body->location.path;
-  int lineno = get_line_no(iseq, cfp->pc - iseq->body->iseq_encoded);
-  VALUE ary = rb_ary_new2(2);
-  rb_ary_push(ary, path);
-  rb_ary_push(ary, INT2NUM(lineno));
-  return ary;
-}
+#define GET_THREAD() \
+  (RTYPEDDATA_DATA(rb_funcall(rb_cThread, rb_intern("current"), 0)))
 
 VALUE
 lol(VALUE self, VALUE depth_v)
@@ -126,27 +80,20 @@ lol(VALUE self, VALUE depth_v)
   Check_Type(depth_v, T_FIXNUM);
   off_t depth = (off_t)FIX2INT(depth_v);
 
-  rb_thread_t *th = get_thread();
-
-  /*                <- first cfp (end control frame)
-   *  top frame (dummy)
-   *  top frame (dummy)
-   *  top frame     <- oldest_cfp
-   *  top frame
-   *  ...
-   *  2nd frame     <- lev:0
-   *  current frame <- th->cfp, newest_cfp
-   */
+  rb_thread_t *th = GET_THREAD();
 
   rb_control_frame_t *oldest_cfp = ((rb_control_frame_t *)((th)->stack + (th)->stack_size)) - 2;
   rb_control_frame_t *newest_cfp = th->cfp;
   rb_control_frame_t *cfp = newest_cfp + depth;
 
-  if (cfp > oldest_cfp) rb_raise(rb_eArgError, "out of bounds");
-  if (cfp->iseq && cfp->pc) return handle_iseq(cfp);
+  VALUE path   = Qnil;
+  VALUE lineno = INT2NUM(0);
 
-  VALUE ary = rb_ary_new2(2);
-  rb_ary_push(ary, Qnil);
-  rb_ary_push(ary, INT2NUM(0));
-  return ary;
+  if (cfp > oldest_cfp) rb_raise(rb_eArgError, "out of bounds");
+  if (cfp->iseq && cfp->pc) {
+    const rb_iseq_t *iseq = cfp->iseq;
+    path = iseq->body->location.path;
+    lineno = INT2NUM(get_line_no(iseq, cfp->pc - iseq->body->iseq_encoded));
+  }
+  return rb_ary_new3(2, path, lineno);
 }
