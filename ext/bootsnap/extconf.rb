dd0a802bfdd4a9ac1b62e4329a03b536e14af928
Burke Libbey <burke@libbey.me>
2017-04-12 19:49:01 -0400

    remove unused crc32 symbol (which also conflicts with zlib)

--- ext/bootsnap/extconf.rb
+++ ext/bootsnap/extconf.rb
@@ -1,6 +1,5 @@
 require "mkmf"
-$CFLAGS << ' -O3 -msse4.2 -std=c99'
-have_header('x86intrin.h')
+$CFLAGS << ' -O3 -std=c99'
 $CFLAGS << ' -Wall -Wextra -Wpedantic -Werror'
 $CFLAGS << ' -Wno-unused-parameter' # VALUE self has to be there but we don't care what it is.
 $CFLAGS << ' -Wno-keyword-macro' # hiding return
