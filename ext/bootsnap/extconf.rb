cb4606f3ceb5c7c19d34fd1a14c0c7e83072e58e
Burke Libbey <burke@libbey.me>
2017-04-27 11:38:43 -0400

    Don't do -wpedantic -werror everywher

--- ext/bootsnap/extconf.rb
+++ ext/bootsnap/extconf.rb
@@ -1,6 +1,17 @@
 require "mkmf"
-$CFLAGS << ' -O3 -std=c99'
-$CFLAGS << ' -Wall -Wextra -Wpedantic -Werror'
-$CFLAGS << ' -Wno-unused-parameter' # VALUE self has to be there but we don't care what it is.
-$CFLAGS << ' -Wno-keyword-macro' # hiding return
+$CFLAGS << ' -O3 '
+$CFLAGS << ' -std=c99'
+$CFLAGS << ' -Wall'
+$CFLAGS << ' -Werror'
+
+# ruby.h has some -Wpedantic fails in some cases
+# (e.g. https://github.com/Shopify/bootsnap/issues/15)
+unless ['0', '', nil].include?(ENV['BOOTSNAP_PEDANTIC'])
+  $CFLAGS << ' -Wextra'
+  $CFLAGS << ' -Wpedantic'
+
+  $CFLAGS << ' -Wno-unused-parameter' # VALUE self has to be there but we don't care what it is.
+  $CFLAGS << ' -Wno-keyword-macro' # hiding return
+end
+
 create_makefile("bootsnap/bootsnap")
