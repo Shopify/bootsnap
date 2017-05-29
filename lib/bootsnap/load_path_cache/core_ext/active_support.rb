00adbd16d6d3821a6cea2ec445b902d0c0653d32
Scott Francis <scott.francis@shopify.com>
2017-05-29 09:18:43 -0400

    Remove load_missing_constant patch

--- lib/bootsnap/load_path_cache/core_ext/active_support.rb
+++ lib/bootsnap/load_path_cache/core_ext/active_support.rb
@@ -29,6 +29,7 @@ module Bootsnap
           def search_for_file(path)
             return super if Thread.current[:without_bootsnap_cache]
             begin
+              path = path.sub(/(\.rb)?$/, ".rb".freeze)
               Bootsnap::LoadPathCache.autoload_paths_cache.find(path)
             rescue Bootsnap::LoadPathCache::ReturnFalse
               nil # doesn't really apply here
@@ -41,16 +42,6 @@ module Bootsnap
 
           def remove_constant(const)
             CoreExt::ActiveSupport.without_bootsnap_cache { super }
-          end
-
-          # If we can't find a constant using the patched implementation of
-          # search_for_file, try again with the default implementation.
-          #
-          # These methods call search_for_file, and we want to modify its
-          # behaviour.  The gymnastics here are a bit awkward, but it prevents
-          # 200+ lines of monkeypatches.
-          def load_missing_constant(from_mod, const_name)
-            CoreExt::ActiveSupport.with_bootsnap_fallback(NameError) { super }
           end
 
           # Signature has changed a few times over the years; easiest to not
