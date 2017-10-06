0cf42f4ad4b23a082f6d449ad3de40b6c4318f28
Burke Libbey <burke@libbey.me>
2017-10-06 13:05:09 -0400

    Revert "Merge pull request #90 from Shopify/revert-88-avoid-double-constant-load"
    This reverts commit a56c0774c7a01c9aaf7cee320768cd03b583cbdc, reversing
    changes made to ed3ef90b7047f01d9cbe7a9e481ccb45f30a2922.

--- lib/bootsnap/load_path_cache/core_ext/active_support.rb
+++ lib/bootsnap/load_path_cache/core_ext/active_support.rb
@@ -2,15 +2,6 @@ module Bootsnap
   module LoadPathCache
     module CoreExt
       module ActiveSupport
-        def self.with_bootsnap_fallback(error)
-          yield
-        rescue error => e
-          # NoMethodError is a NameError, but we only want to handle actual
-          # NameError instances.
-          raise unless e.class == error
-          without_bootsnap_cache { yield }
-        end
-
         def self.without_bootsnap_cache
           prev = Thread.current[:without_bootsnap_cache] || false
           Thread.current[:without_bootsnap_cache] = true
@@ -50,13 +41,21 @@ module Bootsnap
           # behaviour.  The gymnastics here are a bit awkward, but it prevents
           # 200+ lines of monkeypatches.
           def load_missing_constant(from_mod, const_name)
-            CoreExt::ActiveSupport.with_bootsnap_fallback(NameError) { super }
+            super
+          rescue NameError => e
+            # NoMethodError is a NameError, but we only want to handle actual
+            # NameError instances.
+            raise unless e.class == NameError
+            raise if from_mod.const_defined?(const_name)
+            without_bootsnap_cache { super }
           end
 
           # Signature has changed a few times over the years; easiest to not
           # reiterate it with version polymorphism here...
           def depend_on(*)
-            CoreExt::ActiveSupport.with_bootsnap_fallback(LoadError) { super }
+            super
+          rescue LoadError
+            without_bootsnap_cache { super }
           end
         end
       end
