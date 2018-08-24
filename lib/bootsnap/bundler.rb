53f6500b9c21d62546fa3cd0569e9a87487914d7
John Hawthorn <john@hawthorn.email>
2018-08-24 14:47:40 -0700

    Make bundler? false if Bundler isn't defined
    Previously, if bundler environment variables were set, but bundler
    wasn't loaded, bootsnap would fail with
    
        uninitialized constant Bootsnap::LoadPathCache::PathScanner::Bundler
    
    This simply returns false if ::Bundler isn't defined

--- lib/bootsnap/bundler.rb
+++ lib/bootsnap/bundler.rb
@@ -2,11 +2,13 @@ module Bootsnap
   module_function
 
   def bundler?
+    return false unless defined?(::Bundler)
+
     # Bundler environment variable
     ['BUNDLE_BIN_PATH', 'BUNDLE_GEMFILE'].each do |current|
       return true if ENV.key?(current)
     end
-    
+
     false
   end
 end
