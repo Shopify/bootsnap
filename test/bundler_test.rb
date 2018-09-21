3337229c1aa7b26f8b7ceefce255d5a17a62c93a
tommy <kazu9su@gmail.com>
2018-09-21 08:26:22 -0700

    Add without ::Bunder const test case

--- test/bundler_test.rb
+++ test/bundler_test.rb
@@ -4,4 +4,20 @@ class BundlerTest < Minitest::Test
   def test_bundler?
     assert Bootsnap.bundler?
   end
+
+  def without_bundler
+    b = ::Bundler
+    begin
+      Object.send(:remove_const, :Bundler)
+      yield
+    ensure
+      Object.send(:const_set, :Bundler, b)
+    end
+  end
+
+  def test_bundler_when_Bundler_const_not_defined
+    without_bundler do
+      refute_predicate Bootsnap, :bundler?
+    end
+  end
 end
