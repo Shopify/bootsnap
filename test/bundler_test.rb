ebb38b2acd88ea27cd1c59a873156019a4db441c
tommy <kazu9su@gmail.com>
2018-09-21 22:55:00 -0700

    Modify true test case to more concrete
    Modify test case names

--- test/bundler_test.rb
+++ test/bundler_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class BundlerTest < Minitest::Test
-  def test_bundler?
-    assert Bootsnap.bundler?
+  def without_required_env_keys
+    e = {}
+    begin
+      %w[BUNDLE_BIN_PATH BUNDLE_GEMFILE].each do |k|
+        e[k] = ENV[k]
+        ENV[k] = nil
+      end
+      yield
+    ensure
+      e.each { |k, v| ENV[k] = v }
+    end
+  end
+
+  def test_bundler_with_bundle_bin_path_env
+    without_required_env_keys do
+      ENV['BUNDLE_BIN_PATH'] = 'foo'
+      assert_predicate Bootsnap, :bundler?
+    end
+  end
+
+  def test_bundler_with_bundle_gemfile_env
+    without_required_env_keys do
+      ENV['BUNDLE_GEMFILE'] = 'foo'
+      assert_predicate Bootsnap, :bundler?
+    end
   end
 
   def without_bundler
@@ -18,19 +41,6 @@ class BundlerTest < Minitest::Test
   def test_bundler_without_bundler_const
     without_bundler do
       refute_predicate Bootsnap, :bundler?
-    end
-  end
-
-  def without_required_env_keys
-    e = {}
-    begin
-      %w[BUNDLE_BIN_PATH BUNDLE_GEMFILE].each do |k|
-        e[k] = ENV[k]
-        ENV[k] = nil
-      end
-      yield
-    ensure
-      e.each { |k, v| ENV[k] = v }
     end
   end
 
