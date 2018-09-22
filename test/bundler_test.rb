0ad032db9578176a58f6d759a098bb8e73acdf20
tommy <kazu9su@gmail.com>
2018-09-21 22:39:42 -0700

    Add test case without required env keys

--- test/bundler_test.rb
+++ test/bundler_test.rb
@@ -15,8 +15,27 @@ class BundlerTest < Minitest::Test
     end
   end
 
-  def test_bundler_when_Bundler_const_not_defined
+  def test_bundler_without_bundler_const
     without_bundler do
+      refute_predicate Bootsnap, :bundler?
+    end
+  end
+
+  def without_required_env_keys
+    e = {}
+    begin
+      %w[BUNDLE_BIN_PATH BUNDLE_GEMFILE].each do |k|
+        e[k] = ENV[k]
+        ENV[k] = nil
+      end
+      yield
+    ensure
+      e.each { |k, v| ENV[k] = v }
+    end
+  end
+
+  def test_bundler_without_required_env_keys
+    without_required_env_keys do
       refute_predicate Bootsnap, :bundler?
     end
   end
