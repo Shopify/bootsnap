3a9517eb52e175b89f7df55e6a3e505bb9af594a
tommy <kazu9su@gmail.com>
2018-09-25 14:22:45 +0900

    Fix test * confirm Bundler const defined? in test_bundler_without_required_env_keys * define prepared methods for test as private * update valuable name to be more explicit

--- test/bundler_test.rb
+++ test/bundler_test.rb
@@ -1,19 +1,6 @@
 require 'test_helper'
 
 class BundlerTest < Minitest::Test
-  def without_required_env_keys
-    e = {}
-    begin
-      %w[BUNDLE_BIN_PATH BUNDLE_GEMFILE].each do |k|
-        e[k] = ENV[k]
-        ENV[k] = nil
-      end
-      yield
-    ensure
-      e.each { |k, v| ENV[k] = v }
-    end
-  end
-
   def test_bundler_with_bundle_bin_path_env
     without_required_env_keys do
       ENV['BUNDLE_BIN_PATH'] = 'foo'
@@ -28,6 +15,21 @@ class BundlerTest < Minitest::Test
     end
   end
 
+  def test_bundler_without_bundler_const
+    without_bundler do
+      refute_predicate Bootsnap, :bundler?
+    end
+  end
+
+  def test_bundler_without_required_env_keys
+    without_required_env_keys do
+      assert defined?(::Bundler)
+      refute_predicate Bootsnap, :bundler?
+    end
+  end
+
+  private
+
   def without_bundler
     b = ::Bundler
     begin
@@ -38,15 +40,16 @@ class BundlerTest < Minitest::Test
     end
   end
 
-  def test_bundler_without_bundler_const
-    without_bundler do
-      refute_predicate Bootsnap, :bundler?
-    end
-  end
-
-  def test_bundler_without_required_env_keys
-    without_required_env_keys do
-      refute_predicate Bootsnap, :bundler?
+  def without_required_env_keys
+    original_env = {}
+    begin
+      %w[BUNDLE_BIN_PATH BUNDLE_GEMFILE].each do |k|
+        original_env[k] = ENV[k]
+        ENV[k] = nil
+      end
+      yield
+    ensure
+      original_env.each { |k, v| ENV[k] = v }
     end
   end
 end
