1f6a75999c38e893d48fda94cc5a94e53221bbf7
Burke Libbey <burke@libbey.me>
2019-02-06 13:18:23 -0500

    Wno-gcc-compat seems to be necessary on Mojave

--- ext/bootsnap/extconf.rb
+++ ext/bootsnap/extconf.rb
@@ -12,6 +12,7 @@ unless ['0', '', nil].include?(ENV['BOOTSNAP_PEDANTIC'])
 
   $CFLAGS << ' -Wno-unused-parameter' # VALUE self has to be there but we don't care what it is.
   $CFLAGS << ' -Wno-keyword-macro' # hiding return
+  $CFLAGS << ' -Wno-gcc-compat' # ruby.h 2.6.0 on macos 10.14, dunno
 end
 
 create_makefile("bootsnap/bootsnap")
