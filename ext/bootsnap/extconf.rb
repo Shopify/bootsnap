e613d86acc1fbd32f83612aadf96cef44c7a3ac1
Jean Boussier <jean.boussier@gmail.com>
2022-01-11 13:35:18 +0100

    Handle clang 13 warnings

--- ext/bootsnap/extconf.rb
+++ ext/bootsnap/extconf.rb
@@ -16,6 +16,7 @@ if RUBY_ENGINE == 'ruby'
     $CFLAGS << ' -Wno-unused-parameter' # VALUE self has to be there but we don't care what it is.
     $CFLAGS << ' -Wno-keyword-macro' # hiding return
     $CFLAGS << ' -Wno-gcc-compat' # ruby.h 2.6.0 on macos 10.14, dunno
+    $CFLAGS << ' -Wno-compound-token-split-by-macro'
   end
 
   create_makefile("bootsnap/bootsnap")
