acffd63f8f12f6783152187bd1bba9a6d9f64eeb
Randy Stauner <randy.stauner@shopify.com>
2023-06-29 16:27:22 -0700

    Consider truffleruby as supported for yaml/json compile cache
    Tests pass with 21 less skips.
    
    This improves the speed of `Faker::Config.locale` on truffleruby by over 50%.
    
    This moves truffleruby from "minimal" to the full suite in CI.

--- lib/bootsnap/compile_cache.rb
+++ lib/bootsnap/compile_cache.rb
@@ -54,7 +54,7 @@ module Bootsnap
 
     def self.supported?
       # only enable on 'ruby' (MRI), POSIX (darwin, linux, *bsd), Windows (RubyInstaller2) and >= 2.3.0
-      RUBY_ENGINE == "ruby" && RUBY_PLATFORM.match?(/darwin|linux|bsd|mswin|mingw|cygwin/)
+      %w[ruby truffleruby].include?(RUBY_ENGINE) && RUBY_PLATFORM.match?(/darwin|linux|bsd|mswin|mingw|cygwin/)
     end
   end
 end
